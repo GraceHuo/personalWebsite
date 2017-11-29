7417973ef17bebc11080dcc525920b63116fb184
Grace Huo <Grace.Huo@intersystems.com>
2017-11-29 11:04:39 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
