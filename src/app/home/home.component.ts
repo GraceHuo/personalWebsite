e61c60c7db7597598129d2be6d6845766958ee37
Grace Huo <Grace.Huo@intersystems.com>
2017-12-15 09:12:53 -0500

    Add some content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,9 +34,25 @@ export class HomeComponent implements OnInit {
     zip: '02144'
   };
 
+  characteristics = [
+    {
+      name: 'Problem Solver',
+      details: 'My work is full of challenges, from figuring out how to best implement a design, to fixing bugs that crop up.' +
+      'I enjoy these challenges and I always try to solve these problems smartly, quickly and effectively.'
+    },
+    {
+      name: 'Quick Learner',
+      details: 'Tools and techniques used for front-end development change constantly. But I can pick up new skills and apply them quickly.'
+    },
+    {
+      name: 'Team Player',
+      details: 'I enjoy being a part of a team. And my teammate enjoy working with me. ' +
+      'They described me as "reliable, flexible and organized".'
+    }
+  ];
+
   scroll() {
-    console.log(document.getElementsByClassName('best')[0].scrollIntoView);
-    document.getElementsByClassName('best')[0].scrollIntoView();
+    document.getElementsByClassName('best')[0].scrollIntoView({behavior: 'smooth'});
   }
 
   constructor() {
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,221 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  skillLevels: any[] = [
+    {
+      code: 1,
+      name: 'Export',
+      description: 'Skills I am using in daily life'
+    },
+    {
+      code: 2,
+      name: 'Advanced',
+      description: 'Skills I have worked with in some projects'
+    },
+    {
+      code: 3,
+      name: 'Intermediate',
+      description: 'Skills I have played with and have a basic understanding'
+    }
+  ];
 
-  constructor() { }
+  skills: any[] = [
+    {
+      title: 'Front-End Dev Skills',
+      description: 'I am a front-end developer, ' +
+      'the followings are the techniques and tools I use to develop reliable and user-friendly websites.',
+      list: [
+        {
+          category: 'Fundamentals',
+          items: [
+            {
+              name: 'HTML',
+              level: 1
+            },
+            {
+              name: 'CSS',
+              level: 1
+            },
+            {
+              name: 'SASS',
+              level: 1
+            }
+          ]
+        },
+        {
+          category: 'Languages',
+          items: [
+            {
+              name: 'JavaScript',
+              level: 1
+            },
+            {
+              name: 'TypeScript',
+              level: 2
+            },
+            {
+              name: 'Object-Oriented Programming',
+              level: 1
+            },
+            {
+              name: 'Functional Programming',
+              level: 1
+            }
+          ]
+        },
+        {
+          category: 'JavaScript Frameworks',
+          items: [
+            {
+              name: 'AngularJS',
+              level: 1
+            },
+            {
+              name: 'Angular2',
+              level: 2
+            }
+          ]
+        },
+        {
+          category: 'Front-End Frameworks',
+          items: [
+            {
+              name: 'Foundation',
+              level: 1
+            },
+            {
+              name: 'Bootstrap',
+              level: 2
+            }
+          ]
+        },
+        {
+          category: 'JavaScript Libraries (including but not limited to)',
+          items: [
+            {
+              name: 'jQuery',
+              level: 2
+            },
+            {
+              name: 'Lodash / Underscore.js',
+              level: 1
+            },
+            {
+              name: 'HighCharts',
+              level: 2
+            },
+            {
+              name: 'd3.js / c3.js',
+              level: 2
+            },
+            {
+              name: 'moment.js',
+              level: 2
+            }
+          ]
+        },
+        {
+          category: 'Working With Backend',
+          items: [
+            {
+              name: 'RESTful',
+              level: 1
+            },
+            {
+              name: 'JSON',
+              level: 1
+            }
+          ]
+        },
+        {
+          category: 'Design Basic',
+          items: [
+            {
+              name: 'Responsive Design',
+              level: 1
+            }
+          ]
+        },
+        {
+          category: 'Testing',
+          items: [
+            {
+              name: 'Jasmine Framework',
+              level: 1
+            },
+            {
+              name: 'Protractor Framework',
+              level: 3
+            }
+          ]
+        },
+        {
+          category: 'Version Control',
+          items: [
+            {
+              name: 'Git',
+              level: 1
+            },
+          ]
+        },
+        {
+          category: 'Automation',
+          items: [
+            {
+              name: 'Gulp',
+              level: 2
+            },
+          ],
+        }
+      ],
+    },
+    {
+      title: 'Other Software Dev Skills',
+      description: 'I am open to all kinds of new techniques, skills and concept.',
+      list: [
+        {
+          category: 'Languages',
+          items: [
+            {
+              name: 'Java',
+              level: 2
+            },
+            {
+              name: 'Python',
+              level: 2
+            },
+            {
+              name: 'C++ / C',
+              level: 2
+            }
+          ],
+        },
+        {
+          category: 'Others',
+          items: [
+            {
+              name: 'Ruby on Rails',
+              level: 2
+            },
+            {
+              name: 'Android',
+              level: 2
+            },
+            {
+              name: 'Matlab',
+              level: 2
+            }
+          ],
+        }
+      ]
+    }];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
