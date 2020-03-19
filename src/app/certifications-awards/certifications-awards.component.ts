0d2837276dd6778f839436e5d4dcf890f22a85dc
Yan Huo <UA33443@DOW.COM>
2020-03-19 15:48:13 -0500

    update031920

--- src/app/certifications-awards/certifications-awards.component.ts
+++ src/app/certifications-awards/certifications-awards.component.ts
@@ -1,65 +1,68 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-certifications-awards',
-  templateUrl: './certifications-awards.component.html',
-  styleUrls: ['./certifications-awards.component.scss']
+  selector: "app-certifications-awards",
+  templateUrl: "./certifications-awards.component.html",
+  styleUrls: ["./certifications-awards.component.scss"]
 })
 export class CertificationsAwardsComponent implements OnInit {
   certificationsList: any[] = [
     {
-      title: 'Developing with InterSystems Objects and SQLs',
-      from: 'InterSystems',
-      date: '2018'
+      title: "Prosci Change Management",
+      from: "Prosci / Dow",
+      date: "2019"
     },
     {
-      title: 'Building and Managing HL7 Productions',
-      from: 'InterSystems',
-      date: '2017'
+      title: "Developing with InterSystems Objects and SQLs",
+      from: "InterSystems",
+      date: "2018"
     },
     {
-      title: 'Cache Foundations',
-      from: 'InterSystems',
-      date: '2017'
+      title: "Building and Managing HL7 Productions",
+      from: "InterSystems",
+      date: "2017"
     },
     {
-      title: 'HealthShare Information Exchange Fundamentals',
-      from: 'InterSystems',
-      date: '2017'
+      title: "Cache Foundations",
+      from: "InterSystems",
+      date: "2017"
+    },
+    {
+      title: "HealthShare Information Exchange Fundamentals",
+      from: "InterSystems",
+      date: "2017"
     }
   ];
 
   awardsList: any[] = [
     {
-      title: 'First Prize Scholarship of National University Student Innovation Program (Top 1%)',
-      date: '2009'
+      title:
+        "First Prize Scholarship of National University Student Innovation Program (Top 1%)",
+      date: "2009"
     },
     {
-      title: 'Outstanding Student Awards (Top 2%)',
-      date: '2007, 2008 & 2009'
+      title: "Outstanding Student Awards (Top 2%)",
+      date: "2007, 2008 & 2009"
     },
     {
-      title: 'Major Award of Jilin University (Top 5%)',
-      date: '2008 & 2009'
+      title: "Major Award of Jilin University (Top 5%)",
+      date: "2008 & 2009"
     },
     {
-      title: 'Suzhou Gongyeyuan Scholarship',
-      date: '2007'
+      title: "Suzhou Gongyeyuan Scholarship",
+      date: "2007"
     },
     {
-      title: 'Dongrong Scholarship',
-      date: '2007'
+      title: "Dongrong Scholarship",
+      date: "2007"
     },
     {
-      title: 'National Scholarship of China (Top 1%)',
-      date: '2007'
+      title: "National Scholarship of China (Top 1%)",
+      date: "2007"
     }
   ];
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
